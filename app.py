20b5b8214e0a28601906d9df1e5f85d8c73a3c6e
zheng <1762389546@qq.com>
2024-11-18 11:49:02 +0800

    Remove debug print statement in app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from applications import create_app
 from flask_cors import CORS
 app = create_app()
 cors = CORS(app, supports_credentials=True)
-print(1)
+
 if __name__ == '__main__':
     app.run('0.0.0.0', 5010)
