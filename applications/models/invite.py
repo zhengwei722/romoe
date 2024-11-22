fb32ccfcff9393d046a711422536b695a80961bb
zheng <1762389546@qq.com>
2024-11-22 15:30:34 +0800

    Update .gitignore, enhance pay API, add invitation API, fix commission model

--- applications/models/invite.py
+++ applications/models/invite.py
@@ -7,6 +7,7 @@ class InviteRecord(db.Model):
     inviter_id = db.Column(db.Integer, comment='邀请者ID')
     invitee_id = db.Column(db.Integer, comment='受邀者ID')
     isreturnedCash = db.Column(db.Integer,default=0, comment='是否返现') # 0 未返现 1 已返现
+    cashback_amount = db.Column(db.Float, default=0, comment='返现金额')
     created_at = db.Column(db.DateTime, default=datetime.datetime.now, comment='创建时间')
     updated_at = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now, comment='最近更新时间')
 
